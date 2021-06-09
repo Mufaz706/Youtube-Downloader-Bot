d53245da34e38d3a48513d989cc44f528136e4aa
Mufaz706 <83529830+Mufaz706@users.noreply.github.com>
2021-06-09 16:14:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,6 +7,9 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton(" Update Channel", url="https://t.me/Bx_Botz")],[InlineKeyboardButton("Support Group", url="https://t.me/BxSupport")]])
 
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hai <b>{message.from_user.first_name}</b>\nHai ᴍʜᴅ ᴍᴜꜰᴀᴢ!!
+I'm simple YouTube converter I Will Convert Youtube Link to Video/File/Mp3
+
+For more details /help"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
